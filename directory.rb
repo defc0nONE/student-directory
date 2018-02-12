bd3f14ae4d1db944cdc40c7dba45a8a14b28c7ea
Christopher Ly <demedial@gmail.com>
2018-02-12 22:08:11 +0000

    Added a method to save students to an external list, updated menu to reflect this new method

--- directory.rb
+++ directory.rb
@@ -7,29 +7,26 @@ def interactive_menu
   end
 end
 
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "3. Save the list to students.csv"
+  puts "9. Exit"
+end
+
 def process(selection)
   case selection
   when "1"
     @students = input_students
   when "2"
     show_students
+  when "3"
+    save_students
   when "9"
     exit
   else
     puts "I don't know what you meant, try again"
   end
-end
-
-def print_menu
-  puts "1. Input the students"
-  puts "2. Show the students"
-  puts "9. Exit"
-end
-
-def show_students
-  print_header
-  print
-  print_footer
 end
 
 def input_students
@@ -45,6 +42,22 @@ def input_students
   end
 
   @students
+end
+
+def show_students
+  print_header
+  print
+  print_footer
+end
+
+def save_students
+  file = File.open("students.csv", "w")
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
 end
 
 def print_header
